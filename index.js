f5d5d7962a6cee7a46a4019a16a11de8d841b38f
Gleb-Korotunov <korotunov19@gmail.com>
2023-01-04 19:23:21 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
+const express = require('express');
+const cheerio = require('cheerio');
+const axios = require('axios');
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
+})
+app.post('/get_chl/:url',(req,res) =>{
+    console.log(req,res)
+    axios
+        .get('https://whl.ca/gamecentre/1018986/boxscore')
+        .then(response => console.log(response.data))
+        .catch(err => console.log(err))
 })
 app.listen(process.env.PORT || 3000)

3f8c3cf5bdf9452296cf2773194ea3e4a502608e
Gleb-Korotunov <korotunov19@gmail.com>
2023-01-04 20:01:52 +0200

    chl test mode

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 })
 app.post('/get_chl/:url',(req,res) =>{
-    console.log(req,res)
-    axios
-        .get('https://whl.ca/gamecentre/1018986/boxscore')
-        .then(response => console.log(response.data))
-        .catch(err => console.log(err))
+    try{
+        const url = req.params.url;
+        let key = '';
+        let game_id = '';
+        let client_code = '';
+        let jsonRes = {};
+        axios
+            .get(url)
+            .then(response => {
+                const html_data = response.data;
+                const $ = cheerio.load(html_data);
+                client_code = $('#scoreboard').data('league')
+                game_id = url.replace(/[^0-9]+/g, "");
+                key = $('#scoreboard').data('feed_key')
+            })
+            .catch(err => console.log(err))
+        axios
+            .get(`https://cluster.leaguestat.com/feed/index.php?feed=gc&key=${key}&client_code=${client_code}&game_id=${game_id}&lang_code=en&fmt=json&tab=gamesummary`)
+            .then(response => jsonRes = response.data.GC.Gamesummary)
+            .catch(err => console.log(err))
+        return res.status(200).json({
+            ...jsonRes
+        })
+    }catch (err) {
+        return res.status(500).json({
+            err: err.toString(),
+        });
+    }
 })
 app.listen(process.env.PORT || 3000)

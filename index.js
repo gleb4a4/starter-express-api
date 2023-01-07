ba713a4c1339fec3edc8ddeb3ece6ed0dfc2639d
Gleb-Korotunov <korotunov19@gmail.com>
2023-01-07 12:19:04 +0200

    mhl test mode

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.get('/get_mhl', async (req,res)=>{
     try {
         const url = req.query.url;
         const game_id = url.replace(/[^0-9]+/g, "");
-       await axios.get(`http://text.mhl.khl.ru/883370.html`)
+       await axios.get(`http://text.mhl.khl.ru/883400.html`)
             .then(response => {
                 const $ = cheerio.load(response.data)
                  $('.game_info_team > .game_info_team_info > .game_info_team_name').each((iteration,elem) =>{

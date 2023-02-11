ac44fe2f8ed4a1e72094e6f430c3e15822531cae
Gleb-Korotunov <korotunov19@gmail.com>
2023-02-11 22:55:29 +0200

    NHL  test mode

--- index.js
+++ index.js
@@ -408,7 +408,9 @@ app.get('/get_nhl_events_match', async (req,res) => {
             Bucket: "cyclic-fine-tan-snapper-ring-eu-west-1",
             Key: `nhl_games_${currentDate}.json`,
         }).promise()
-
+        return res.status(200).json({
+            ...JSON.parse(nhl_games)
+        })
                 let obj = JSON.parse(nhl_games); //now it an object
                 for (const [key, _] of Object.entries(obj)) {
                     const { data } = await axios.get(`https://statsapi.web.nhl.com/api/v1/game/${key}/feed/live`)
